9a5840c9e7a6e7f31cae25737073f8fed1db3ff0
thiyangt <thiyanga.talagala@monash.edu>
2017-12-05 10:47:42 +1100

    added a unit test for acf_features

---
+++ tests/testthat/test-acf_features.R
@@ -0,0 +1,29 @@
+# A unit tests for acf_features() function
+
+if(require(testthat))
+{
+  context("Tests on output")
+  test_that("test for acf_features() results on non-seasonal ts data", {
+    z <- acf_features(WWWusage)
+    expect_equal(length(z), 6L)
+    expect_gt(z[1], 0.96)
+    expect_gt(z[2], 4.19)
+    expect_gt(z[3], 0.79)
+    expect_gt(z[4], 1.40)
+    expect_gt(z[5], 0.86)
+    expect_gt(z[6], 1.78)
+  })
+  test_that("test for acf_features() results on seasonal ts data", {
+    z <- acf_features(USAccDeaths)
+    expect_equal(length(z), 7L)
+    expect_gt(z[1], 0.70)
+    expect_gt(z[2], 1.20)
+    expect_gt(z[3], 0.02)
+    expect_gt(z[4], 0.27)
+    expect_gt(z[5], 0.53)
+    expect_gt(z[6], 0.77)
+    expect_gt(z[7], 0.62)
+  })
+  
+}
+
