b8c9a3a86ef9efb612fbcb812a422afa81569f28
thiyangt <thiyanga.talagala@monash.edu>
2017-12-05 11:00:06 +1100

    added unit tests for holt_parameters() and pacf_features() functions

---
+++ tests/testthat/test-pacf_features.R
@@ -0,0 +1,23 @@
+# A unit tests for pacf_features() function
+
+if(require(testthat))
+{
+  context("Tests on output")
+  test_that("test for pacf_features() results on non-seasonal ts data", {
+    z <- pacf_features(WWWusage)
+    expect_equal(length(z), 3L)
+    expect_gt(z[1], 1.03)
+    expect_gt(z[2], 0.80)
+    expect_gt(z[3], 1.30)
+  })
+  test_that("test for pacf_features() results on seasonal ts data", {
+    z <- pacf_features(USAccDeaths)
+    expect_equal(length(z), 4L)
+    expect_gt(z[1], 0.63)
+    expect_gt(z[2], 0.09)
+    expect_gt(z[3], 0.48)
+    expect_gt(z[4], 0.12)
+  })
+  
+}
+
