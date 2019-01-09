8e59105fe5b83ea3bc2f7779f15b9f074e202534
Rob J Hyndman <robjhyndman@gmail.com>
2019-01-09 13:28:28 +1100

    Added Mitch as author of yahoo_data

--- R/yahoo.R
+++ R/yahoo.R
@@ -5,7 +5,7 @@
 #' Downloads and returns aggregated and anonymized datasets from Yahoo representing server metrics of Yahoo services.
 #'
 #' @return A matrix of time series with 1437 rows of hourly data, and 1748 columns representing different servers.
-#' @author Rob Hyndman, Earo Wang, Nikolay Laptev
+#' @author Rob Hyndman, Earo Wang, Nikolay Laptev, Mitchell O'Hara-Wild
 #' @references
 #' Hyndman, R.J., Wang, E., Laptev, N. (2015) Large-scale unusual time series detection.
 #' In: \emph{Proceedings of the IEEE International Conference on Data Mining}. Atlantic City, NJ, USA. 14–17 November 2015.
