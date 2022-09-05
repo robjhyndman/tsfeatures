b843b146c60d1a13c8ff8b442b04d82affb68483
Rob J Hyndman <robjhyndman@gmail.com>
2022-09-05 11:46:34 +0200

    Fixed references for nonlinearity test

--- R/yanfei.R
+++ R/yanfei.R
@@ -53,15 +53,17 @@ heterogeneity <- function(x) {
 
 #' Nonlinearity coefficient
 #'
-#' Computes a nonlinearity statistic based on Teräsvirta's nonlinearity test of a time series.
-#' The statistic is \eqn{10X^2/T}{10X^2/T} where \eqn{X^2}{X^2} is the Chi-squared statistic from
-#' Teräsvirta's test, and T is the length of the time series. This takes large values
+#' Computes a nonlinearity statistic based on Lee, White & Granger's nonlinearity test of a time series.
+#' The statistic is \eqn{10X^2/T}{10X^2/T} where \eqn{X^2}{X^2} is the Chi-squared statistic from Lee, White and Granger,
+#' and T is the length of the time series. This takes large values
 #' when the series is nonlinear, and values around 0 when the series is linear.
 #' @param x a univariate time series
 #' @return A numeric value.
 #' @examples
 #' nonlinearity(lynx)
 #' @author Yanfei Kang and Rob J Hyndman
+#' @references Lee, T. H., White, H., & Granger, C. W. (1993). Testing for neglected nonlinearity in time series models: A comparison of neural network methods and alternative tests. \emph{Journal of Econometrics}, 56(3), 269-290.
+#' @references Teräsvirta, T., Lin, C.-F., & Granger, C. W. J. (1993). Power of the neural network linearity test. \emph{Journal of Time Series Analysis}, 14(2), 209–220.
 #' @export
 
 nonlinearity <- function(x) {
