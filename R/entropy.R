464b4cea2648c55889e0e611020ff22031f25490
Rob Hyndman <robjhyndman@gmail.com>
2022-10-08 13:42:32 +1100

    Fixed URLs

--- R/entropy.R
+++ R/entropy.R
@@ -1,34 +1,34 @@
 #' Spectral entropy of a time series
 #'
 #' @description
-#' Computes spectral entropy from a univariate normalized 
+#' Computes spectral entropy from a univariate normalized
 #' spectral density, estimated using an AR model.
-#' 
+#'
 #' @details
 #' The \emph{spectral entropy} equals the Shannon entropy of the spectral density
-#' \eqn{f_x(\lambda)} of a stationary process \eqn{x_t}: 
-#' \deqn{ 
-#' H_s(x_t) = - \int_{-\pi}^{\pi} f_x(\lambda) \log f_x(\lambda) d \lambda, 
+#' \eqn{f_x(\lambda)} of a stationary process \eqn{x_t}:
+#' \deqn{
+#' H_s(x_t) = - \int_{-\pi}^{\pi} f_x(\lambda) \log f_x(\lambda) d \lambda,
 #' }
-#' where the density is normalized such that 
-#' \eqn{\int_{-\pi}^{\pi} f_x(\lambda) d \lambda = 1}. 
+#' where the density is normalized such that
+#' \eqn{\int_{-\pi}^{\pi} f_x(\lambda) d \lambda = 1}.
 #' An estimate of \eqn{f(\lambda)} can be obtained using \code{\link[stats]{spec.ar}} with
 #' the `burg` method.
-#' 
+#'
 #' @param x a univariate time series
 #' @author Rob J Hyndman
-#' @return 
+#' @return
 #' A non-negative real value for the spectral entropy \eqn{H_s(x_t)}.
 #' @seealso \code{\link[stats]{spec.ar}}
-#' @references 
+#' @references
 #' Jerry D. Gibson and Jaewoo Jung (2006). \dQuote{The
 #' Interpretation of Spectral Entropy Based Upon Rate Distortion Functions}.
 #' IEEE International Symposium on Information Theory, pp. 277-281.
-#' 
+#'
 #' Goerg, G. M. (2013). \dQuote{Forecastable Component Analysis}.
-#' Journal of Machine Learning Research (JMLR) W&CP 28 (2): 64-72, 2013.
-#' Available at \url{http://jmlr.org/proceedings/papers/v28/goerg13.html}.
-#' 
+#' Proceedings of the 30th International Conference on Machine Learning (PMLR) 28 (2): 64-72, 2013.
+#' Available at \url{https://proceedings.mlr.press/v28/goerg13.html}.
+#'
 #' @examples
 #' entropy(rnorm(1000))
 #' entropy(lynx)
